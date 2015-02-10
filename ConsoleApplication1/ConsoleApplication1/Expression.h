278502924c385aa70f2f2f70ce666679db149cc6
Florian GUIF <myemail@gmail.com>
2015-02-10 15:55:30 +0100

    expression était dans les fichiers exlue why not

--- ConsoleApplication1/ConsoleApplication1/Expression.h
+++ ConsoleApplication1/ConsoleApplication1/Expression.h
@@ -2,16 +2,23 @@
 #define EXPRESSION
 #include<set>
 #include <iostream>
+#include "TypeEnum.h"
 #include <string>
 using namespace std;
+
+
 class Expression
 {
+
+
 public:
 	Expression();
 
 	virtual const double eval()=0;
 	virtual string const affiche() = 0;
+	virtual Type getType();
 	virtual Expression * derive(string var);
+	virtual Expression * simplifier();
 	static void toutLiberer();
 	virtual ~Expression();
 	friend ostream& operator<< (ostream &out, Expression &e);
